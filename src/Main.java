3fe03ebe56256e95199a1ab0b22b534c3c9fc7fd
dio <pamelacristinajj@gmail.com>
2023-01-05 17:22:29 -0300

    Curso e Mentoria.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
 
-public class Main {
-public static void main(String [] args) {
+ public class Main {
+ public static void main (String[] args) {
     Curso curso1 = new Curso();
     curso1.setTitulo("curso java");
     curso1.setDescricao("descricao curso java");
